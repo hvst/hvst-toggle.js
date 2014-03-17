fd8d3050ddaa8210c6caa7f8670b167344e6cb7d
Enrique Alcantara <enriquealc27@gmail.com>
2014-03-17 19:59:57 -0400

    set up karma for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,6 +55,12 @@ module.exports = function(grunt) {
         src: ['lib/**/*.js', 'test/**/*.js']
       }
     },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js',
+        background : true
+      }
+    },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
@@ -62,7 +68,7 @@ module.exports = function(grunt) {
       },
       lib_test: {
         files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test']
+        tasks: ['jshint:lib_test', 'karma:unit:run']
       }
     }
   });
@@ -71,6 +77,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
